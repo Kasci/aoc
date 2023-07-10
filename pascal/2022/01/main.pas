8ab51d4c3416597335854e45eb493531470a5bdd
Jozef Kaščák <kascak.jozef@gmail.com>
2023-07-10 21:48:42 +0200

    pas/2022/01/p2

--- pascal/2022/01/main.pas
+++ pascal/2022/01/main.pas
@@ -2,6 +2,9 @@ program main;
 uses 
 	util, sysutils;
 
+type
+	m_arr = array [0..2] of longint;
+
 const
 	debug = false;
 
@@ -40,14 +43,41 @@ var
 		WriteLn('Part 1: ',i);
 	end;
 
+	procedure add(v: longint; var max: m_arr);
+	begin
+(*		writeln('>',v,' ',max[0],' ',max[1],' ',max[2]);*)
+		if v > max[0] then begin
+			max[2] := max[1];
+			max[1] := max[0];
+			max[0] := v;
+		end else if v > max[1] then begin
+			max[2] := max[1];
+			max[1] := v;
+		end else if v > max[2] then begin
+			max[2] := v;
+		end;
+	end;
+
 	procedure part2;
 	var 
 		A: array of string;
-		i: integer;	
+		i, tmp: longint;	
+		max: array [0..2] of longint = (0,0,0);
 		aa: string;
 	begin		
 		A := util.readInput(f);
 		i := 0;
+		tmp := 0;
+		for aa in A do begin
+			if aa = '' then begin
+				add(tmp, max);
+				tmp := 0;
+			end else begin
+				tmp += StrToInt(aa);
+			end
+		end;
+		add(tmp, max);
+		i := max[0]+max[1]+max[2];
 		WriteLn('Part 2: ',i);
 	end;
 begin
@@ -58,6 +88,6 @@ begin
 	assign(f, fp);
 
 	part1;
-	(* part2; *)
+	part2;
 
 end.
