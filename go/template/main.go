89bb7b84c9e188a24cdd74728ab2c455bb4fdc20
Jozef Kaščák <kascak.jozef@gmail.com>
2024-10-19 13:55:44 +0200

    added go template

---
+++ go/template/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	s "strings"
+)
+
+func isDebug() bool {
+	return true
+}
+
+func getInput() []string {
+	name := "./input.txt"
+	if isDebug() {
+		name = "./sample.txt"
+	}
+	dat, err := os.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+	return s.Split(string(dat), "\n")
+}
+
+func getIntInput() []int {
+	input := getInput()
+	list := make([]int, len(input))
+	for i, val := range input {
+		a, err := strconv.Atoi(val)
+		if err != nil {panic(err)}
+		list[i] = a
+	}
+	return list
+}
+
+func main() {
+	part1()
+//	part2()
+}
+
+func part1() {
+	ret := 0
+	input := getInput()
+	for _, val := range input {
+		fmt.Println(val)
+	}
+
+	fmt.Println("Part 1:", ret)
+}
+
+func part2() {
+	ret := 0
+	input := getInput()
+	for _, val := range input {
+		fmt.Println(val)
+	}
+
+	fmt.Println("Part 2:", ret)
+}
